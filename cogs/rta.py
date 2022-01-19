a1f9f304869cc7acaf6cd7840407f865a2cccba8
justink6 <justink6@illinois.edu>
2022-01-19 16:33:09 -0600

    rta

--- cogs/rta.py
+++ cogs/rta.py
@@ -3,6 +3,8 @@ import discord
 from discord.ext import commands
 from .db import db
 
+CURRENT_SEASON = 6
+
 class RTA(commands.Cog):
   def __init__(self, client):
     self.client = client
@@ -16,19 +18,17 @@ class RTA(commands.Cog):
     return n
 
   # Get the overall win rate of unit
-  def getOverallWinrate(self, name, count):
-    # Overall count of matches won with unit
-    query = """SELECT COUNT(match_id)
-               FROM RTA
-               WHERE NOT (p1_ban = ? OR p2_ban = ?) AND
-                     (((p1_pick1 = ? OR p1_pick2 = ? OR p1_pick3 = ? OR p1_pick4 = ? OR p1_pick5 = ?) AND match_winner = 1) OR 
-                     ((p2_pick1 = ? OR p2_pick2 = ? OR p2_pick3 = ? OR p2_pick4 = ? OR p2_pick5 = ?) AND match_winner = 2))"""
+  def getOverallWinrate(self, name, season = CURRENT_SEASON):
+    query = "SELECT * FROM RTAStats WHERE name = ? AND season = ?"
 
-    won = db.fetch(query, name, name, name, name, name, name, name, name, name, name, name, name)[0][0]
-    return won / count
+    data = db.fetch(query, name, season)
+    if len(data) == 0:
+      return 0
+
+    return data[0][4] / (data[0][2] - data[0][3])
 
   # Get the overall preban rate of unit
-  def getOverallPrebanRate(self, name, count):
+  def getOverallPrebanRate(self, name, totalCount):
     # Overall count of matches with unit prebanned
     query = """SELECT COUNT(match_id)
                FROM RTA
@@ -36,30 +36,27 @@ class RTA(commands.Cog):
 
     prebans = db.fetch(query, name, name)[0][0]
 
-    query = "SELECT COUNT(match_id) FROM RTA"
-
-    totalCount = db.fetch(query)[0][0]
-
     return prebans / totalCount
 
   # Get the general postban rate of unit
-  def getOverallPostbanRate(self, name):
-    # Overall count of matches with character chosen
-    query = """SELECT COUNT(match_id)
-               FROM RTA
-               WHERE (p1_pick1 = ? OR p1_pick2 = ? OR p1_pick3 = ? OR p1_pick4 = ? OR p1_pick5 = ?) OR 
-                     (p2_pick1 = ? OR p2_pick2 = ? OR p2_pick3 = ? OR p2_pick4 = ? OR p2_pick5 = ?)"""
+  def getOverallPostbanRate(self, name, season = CURRENT_SEASON):
+    query = "SELECT * FROM RTAStats WHERE name = ? AND season = ?"
 
-    count = db.fetch(query, name, name, name, name, name, name, name, name, name, name)[0][0]
+    data = db.fetch(query, name, season)
+    if len(data) == 0:
+      return 0
 
-    # Overall count of matches with unit postbanned
-    query = """SELECT COUNT(match_id)
-               FROM RTA
-               WHERE p1_ban = ? OR p2_ban = ?"""
+    return data[0][3] / data[0][2]
 
-    postbans = db.fetch(query, name, name)[0][0]
+  # Get the first pick rate of unit
+  def getFirstPickRate(self, name, totalCount, season = CURRENT_SEASON):
+    query = "SELECT * FROM RTAStats WHERE name = ? AND season = ?"
 
-    return postbans / count
+    data = db.fetch(query, name, season)
+    if len(data) == 0:
+      return 0
+
+    return data[0][5] / totalCount
 
   # Gets wins and losses
   def getWinLoss(self, name):
@@ -126,12 +123,44 @@ class RTA(commands.Cog):
 
     return (wins,losses)
 
-  # Gets the units the character has the lowest winrates against
-  def getLosesAgainst(self, name):
-    return self.getWinLoss(name)
+  # Calculates winrates against characters
+  def calculateWinRates(self, name):
+    # Get number of wins/losses vs other chars
+    data = self.getWinLoss(name)
+
+    wins = data[0]
+    losses = data[1]
+
+    # Calculate winrates for each character
+    winrates = {}
+    for unit in wins:
+      numWins = wins[unit]
+      numLosses = losses[unit]
+
+      if numWins == 0 and numLosses == 0:
+        continue
+
+      winRate = numWins / (numWins + numLosses)
+      if winRate > 0.0:
+        winrates[unit] = numWins / (numWins + numLosses)
+
+    return dict(sorted(winrates.items(), key=lambda item: item[1]))
+
+  # Gets the units the character has the lowest winrates against (at most 3)
+  def worstMatchups(self, name):
+    winRates = self.calculateWinRates(name)
+    result = {}
+
+    for index, wr in enumerate(winRates):
+      if index > 2:
+        break
+
+      result[wr] = winRates[wr]
+
+    return result
   
   # Create embed to send based on data
-  def getEmbed(self, name, overallWinrate, prebanRate, postbanRate):
+  def getEmbed(self, name, overallWinrate, prebanRate, postbanRate, firstPickRate, worstMatchups):
     embed = discord.Embed(
       title = f"RTA Stats for {name}",
     )
@@ -140,10 +169,20 @@ class RTA(commands.Cog):
     imageLink = db.fetch(query, name)[0][0]
 
     embed.set_thumbnail(url=imageLink)
+    embed.add_field(name = "Winrate:", value = f"{overallWinrate}%", inline = False)
+    embed.add_field(name = "Preban Rate:", value = f"{prebanRate}%", inline = True)
+    embed.add_field(name = "Postban Rate:", value = f"{postbanRate}%", inline = True)
+    embed.add_field(name = "First Pick Rate:", value = f"{firstPickRate}%", inline = True)
 
-    embed.add_field(name = "Overall Win Rate:", value = f"{overallWinrate}%", inline = False)
-    embed.add_field(name = "Overall Preban Rate:", value = f"{prebanRate}%", inline = False)
-    embed.add_field(name = "Overall Postban Rate:", value = f"{postbanRate}%", inline = False)
+    # Worst matchup info
+    matchupString = ""
+    if len(worstMatchups) == 0:
+      matchupString = "N/A"
+      
+    for matchup in worstMatchups:
+      matchupString += f"{matchup} ({round(worstMatchups[matchup] * 100, 2)}%)\n"
+
+    embed.add_field(name = "Struggles against (Win%):", value = matchupString, inline = False)
 
     return embed
 
@@ -158,33 +197,26 @@ class RTA(commands.Cog):
 
     nameString = name[0][0]
 
-    # Overall count of matches done with character
-    query = """SELECT COUNT(match_id)
-               FROM RTA
-               WHERE NOT (p1_ban = ? OR p2_ban = ?) AND
-                     ((p1_pick1 = ? OR p1_pick2 = ? OR p1_pick3 = ? OR p1_pick4 = ? OR p1_pick5 = ?) OR 
-                     (p2_pick1 = ? OR p2_pick2 = ? OR p2_pick3 = ? OR p2_pick4 = ? OR p2_pick5 = ?))"""
+    # Get total number of matches
+    query = "SELECT COUNT(match_id) FROM RTA"
+    totalCount = db.fetch(query)[0][0]
 
-    count = db.fetch(query, nameString, nameString, nameString, nameString, nameString, nameString, nameString, nameString, nameString, nameString, nameString, nameString)[0][0]
+    # Get overall winrate of character
+    overallWinrate = round(self.getOverallWinrate(nameString) * 100, 2)
 
-    overallWinrate = 0.0
-    postbanRate = 0.0
-
-    if count != 0:
-      # Get overall winrate of character
-      overallWinrate = round(self.getOverallWinrate(nameString, count) * 100, 2)
-
-      # Get postban rate of character
-      postbanRate = round(self.getOverallPostbanRate(nameString) * 100, 2)
+    # Get postban rate of character
+    postbanRate = round(self.getOverallPostbanRate(nameString) * 100, 2)
 
     # Get preban rate of character
-    prebanRate = round(self.getOverallPrebanRate(nameString, count) * 100, 2)
+    prebanRate = round(self.getOverallPrebanRate(nameString, totalCount) * 100, 2)
 
-    # Get loss rate of character
-    lossRate = self.getLosesAgainst(nameString)
-    print(f"Wins: {lossRate[0]}\nLosses: {lossRate[1]}")
+    # Get the first pick rate of the character
+    firstPickRate = round(self.getFirstPickRate(nameString, totalCount) * 100, 2)
 
-    await ctx.send(embed = self.getEmbed(nameString, overallWinrate, prebanRate, postbanRate)) 
+    # Get worst matchups
+    worstMatchups = self.worstMatchups(nameString)
+
+    await ctx.send(embed = self.getEmbed(nameString, overallWinrate, prebanRate, postbanRate, firstPickRate, worstMatchups)) 
 
 def setup(client):
   client.add_cog(RTA(client)) 
